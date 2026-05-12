62004b39337451983a5aef5a40d91a08bfe1855d
Karyna <karymashei@gmail.com>
2026-05-12 22:59:43 +0200

    Homework1

--- src/main/java/MethodsArray1.java
+++ src/main/java/MethodsArray1.java
@@ -1,24 +1,24 @@
 public class MethodsArray1 {
 
     public static void main(String[] args) {
-        // 1. Проверяем byte
+        // 1.
         byte[] ar1 = {3, 127, 5, -15, 8};
-        System.out.println("Sum byte: " + arraySum(ar1));
+        System.out.println(arraySum(ar1));
 
-        // 2. Проверяем double
+        // 2.
         double[] ar2 = {8.3, 7.23, 9.12, -23.3, -2.1};
-        System.out.println("Sum double: " + arraySum(ar2));
+        System.out.println(arraySum(ar2));
 
-        // 3. Проверяем float
+        // 3.
         float[] ar3 = {6.2F, 5.7F, 8.4F};
-        System.out.println("Sum float: " + arraySum(ar3));
+        System.out.println(arraySum(ar3));
 
-        // 4. Проверяем boolean (ADVANCE)
+        // 4.
         boolean[] ar4 = {true, false, true, false};
-        System.out.println("Count true: " + arraySum(ar4));
+        System.out.println(arraySum(ar4));
     }
 
-    // Твои методы:
+    // HW 1
     public static int arraySum(byte[] ar) {
         int sum = 0;
         for (int i = 0; i < ar.length; i++) {
@@ -27,6 +27,7 @@ public class MethodsArray1 {
         return sum;
     }
 
+    // HW 2
     public static double arraySum(double[] ar) {
         double sum = 0;
         for (int i = 0; i < ar.length; i++) {
@@ -35,6 +36,7 @@ public class MethodsArray1 {
         return sum;
     }
 
+    // HW 3
     public static double arraySum(float[] ar) {
         double sum = 0;
         for (int i = 0; i < ar.length; i++) {
@@ -43,6 +45,7 @@ public class MethodsArray1 {
         return sum;
     }
 
+    // HW 4 ADVANCE
     public static byte arraySum(boolean[] ar) {
         byte sum = 0;
         for (int i = 0; i < ar.length; i++) {
